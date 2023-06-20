f88ab691dcd7035386ef4e4fdbf815f9f7edcf21
gitarplayer <51920729+GitarPlayer@users.noreply.github.com>
2023-06-20 15:29:09 +0200

    set {} to null

--- src/main.rs
+++ src/main.rs
@@ -19,43 +19,20 @@ fn clean_value(val: &Value) -> Option<Value> {
             }
         },
         Value::Object(map) => {
-            let cleaned: Map<String, Value> = map.iter()
-                .filter_map(|(k, v)| clean_value(v).map(|v| (k.trim().to_owned(), v)))
-                .collect();
-            Some(Value::Object(cleaned))
+            if map.is_empty() {
+                Some(Value::Null)
+            } else {
+                let cleaned: Map<String, Value> = map.iter()
+                    .filter_map(|(k, v)| clean_value(v).map(|v| (k.trim().to_owned(), v)))
+                    .collect();
+                Some(Value::Object(cleaned))
+            }
         },
         _ => Some(val.clone()),
     }
 }
 
-fn clean_json(json: &str) -> Result<String, Box<dyn std::error::Error>> {
-    let value: Value = serde_json::from_str(json)?;
-
-    if value.is_object() && value.as_object().unwrap().is_empty() {
-        return Err("JSON is an empty object".into());
-    }
-
-    let cleaned = clean_value(&value);
-    match cleaned {
-        Some(v) => Ok(serde_json::to_string(&v)?),
-        None => Err("Cleaned JSON is empty".into()),
-    }
-}
-
-fn main() {
-    let mut buffer = String::new();
-    if let Err(e) = std::io::stdin().read_to_string(&mut buffer) {
-        eprintln!("Error reading input: {}", e);
-        std::process::exit(1);
-    }
-    match clean_json(&buffer) {
-        Ok(json) => println!("{}", json),
-        Err(e) => {
-            eprintln!("Error cleaning json: {}", e);
-            std::process::exit(1);
-        },
-    }
-}
+// Rest of the main function code...
 
 #[cfg(test)]
 mod tests {
@@ -79,7 +56,7 @@ mod tests {
             }
         }
         "#;
-        let expected = r#"{"key":"true","empty array":null,"empty object":{},"empty string":"","nested":{"key":"false","empty array":null,"empty object":{},"empty string":""}}"#;
+        let expected = r#"{"key":"true","empty array":null,"empty object":null,"empty string":"","nested":{"key":"false","empty array":null,"empty object":null,"empty string":""}}"#;
         let cleaned = clean_json(input).unwrap();
 
         let cleaned_value: Value = serde_json::from_str(&cleaned).unwrap();
